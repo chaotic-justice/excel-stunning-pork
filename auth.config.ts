b61703be1861e7d2df733fdd6bb7b7545f44e2cb
chaotic-justice <deepocean.2900@gmail.com>
2025-02-01 11:31:11 -0500

    remove callbacks

--- auth.config.ts
+++ auth.config.ts
@@ -5,26 +5,26 @@ export default {
   session: {
     strategy: "jwt",
   },
-  callbacks: {
-    // @ts-ignore
-    async signIn({ user, account }) {
-      //Allow OAuth without email verification
-      if (account?.provider !== "credentials") return true
-      // email verification logic here
-      return true
-    },
-    // @ts-ignore
-    async session({ token, session }) {
-      if (token.userId && session.user) {
-        session.user.id = token.userId as string
-      }
+  // callbacks: {
+  //   // @ts-ignore
+  //   async signIn({ user, account }) {
+  //     //Allow OAuth without email verification
+  //     if (account?.provider !== "credentials") return true
+  //     // email verification logic here
+  //     return true
+  //   },
+  //   // @ts-ignore
+  //   async session({ token, session }) {
+  //     if (token.userId && session.user) {
+  //       session.user.id = token.userId as string
+  //     }
 
-      return session
-    },
-    async jwt({ token }: { token: any }) {
-      return token
-    },
-  },
+  //     return session
+  //   },
+  //   async jwt({ token }: { token: any }) {
+  //     return token
+  //   },
+  // },
   providers: [
     Google({
       authorization: { params: { access_type: "offline", prompt: "consent" } },
